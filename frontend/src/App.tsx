3dd830c0cb0f0858cf46e7081adbffaf867357dc
ryan <ryannesh2376@gmail.com>
2025-05-29 15:19:53 +0300

    routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/Home';
+import TeachersPage from './pages/Teachers';
+import Navbar from './components/Navbar';
 
-  function App() {
+function App() {
   return (
-    <div className="p-6 text-center">
-      <h1 className="text-4xl font-bold text-blue-600">Smart Timetable</h1>
-      <p className="text-gray-600">React + Vite + TypeScript + Tailwind 💡</p>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/teachers" element={<TeachersPage />} />
+      </Routes>
+    </Router>
   );
 }
 
-export default App
+export default App;
