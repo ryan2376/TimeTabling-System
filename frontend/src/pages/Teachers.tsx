4eda6e4a7a322c72423b2f33bc37bc6b91dc9fea
ryan <ryannesh2376@gmail.com>
2025-06-03 12:13:14 +0300

    begin

--- frontend/src/pages/Teachers.tsx
+++
@@ -1,8 +0,0 @@
-export default function TeachersPage() {
-    return (
-        <div className="p-6">
-            <h2 className="text-xl font-semibold">Teachers</h2>
-            <p>Here you'll manage all the teachers.</p>
-        </div>
-    );
-}
