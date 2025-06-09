1ca4a7a3b5ef62a2fd48b935c1bbb348ccf97609
ryan <ryannesh2376@gmail.com>
2025-06-09 16:24:31 +0300

    express

---
+++ Backend/src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use()
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
+});
