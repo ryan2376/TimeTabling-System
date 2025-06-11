bfaed55b5988366a2850baa397b3622c6ae88e3a
ryan <ryannesh2376@gmail.com>
2025-06-11 11:55:24 +0300

    backend setup

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,17 +1,27 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
+import pool from './db';
+// import dotenv from 'dotenv';
 
 
-dotenv.config();
+// dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 app.use(express.json());
 
-app.use()
+// connection test
+pool.connect()
+    .then(() => console.log('Connected to PostgreSQL database'))
+    .catch(err => console.error('PostgreSQL connection error', err))
+
+app.get('/', (req, res) => {
+    res.send('Server is up and running');
+});
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
